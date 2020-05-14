6782eac7d520bac217fac93194b60ff5a06a851f
Anisha2002 <45850205+Anisha2002@users.noreply.github.com>
2020-05-14 10:30:44 +0200

    Made contact page and controller

---
+++ app/Http/Controllers/Contactcontroller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Contactcontroller extends Controller
+{
+    public function contact(){
+        return view ('contact');
+    }
+}
